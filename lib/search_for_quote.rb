69b898a7f8ca105d3bf4483e2a6961149b8afdc7
Tim Salazar <tim.salazar@gmail.com>
2011-11-01 18:08:33 -0700

    Fixes for search_for_quote

--- lib/search_for_quote.rb
+++ lib/search_for_quote.rb
@@ -7,3 +7,23 @@
 # @see http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
 # @see Programing Ruby, Chapter 6 
 #
+def search_for_quote(criteria)
+  quotes = all_quotes(criteria.delete(:file))
+  results = criteria.map {|criterion| quotes.grep(create_regexp(*criterion))}.flatten
+
+  if results.empty?
+    quotes
+  else
+    results
+  end
+end
+
+def create_regexp(criterion, text)
+  case criterion
+    when :start_with then %r(^#{text})
+    when :end_with   then %r(#{text}$)
+    when :include    then %r(#{text})
+  end
+end
+
+                   
